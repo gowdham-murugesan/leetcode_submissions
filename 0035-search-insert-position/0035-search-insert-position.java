fb729875815c84d9249c6ee2c20053a8e217348e
Gowdham M <gowdhammurugesh24@gmail.com>
2024-01-30 20:02:57 +0530

    Time: 0 ms (100%), Space: 42.7 MB (89.05%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,12 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        for(int i=0; i<nums.length; i++)
-        {
-            if(target<=nums[i])
+    public int searchInsert(int[] n, int target) {
+        int a=0;
+        for(int i=0;i<n.length;i++){
+            if(target<=n[i]){
                 return i;
+            }
         }
-        return nums.length;
+        return n.length;
+        
     }
 }
